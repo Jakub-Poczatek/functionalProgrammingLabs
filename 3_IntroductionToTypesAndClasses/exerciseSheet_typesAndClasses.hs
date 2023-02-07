7558be920b42e6e0512768c2f00beeb4a521ee89
Jakub Poczatek <kubapoczatek2001@gmail.com>
2023-02-07 22:36:25 +0000

    Added solutions to exercise sheets
    Signed-off-by: Jakub Poczatek <kubapoczatek2001@gmail.com>

--- 3_IntroductionToTypesAndClasses/exerciseSheet_typesAndClasses.hs
+++ 3_IntroductionToTypesAndClasses/exerciseSheet_typesAndClasses.hs
@@ -1,53 +1,53 @@
 -- Exercise 1
 
-[a, 'b', 'c']
-Invalid
+['a', 'b', 'c']
+[Char] -- Correct
 
 ('a', 'b', 'c')
-(Char, Char, Char)
+(Char, Char, Char) -- Correct
 
 [(False, '0'), (True, '1')]
-[(Bool, Char)]
+[(Bool, Char)] -- Correct
 
 (['1', '0'], ['0', '1'])
-([Char], [Char])
+([Char], [Char]) -- Correct
 
 [tail, init, reverse]
-[[a] -> [a]]  
+[[a] -> [a]] -- Correct
 
 -- Exercise 2
 
 bools :: [Bool]
-let bools = [True, False]
+let bools = [True, False] -- Correct
 
 nums :: [[Int]]
-let nums = [[1, 2], [3, 4]]
+let nums = [[1, 2], [3, 4]] -- Correct
 
 add :: Int -> Int -> Int -> Int
-add a b c = a + b + c
+add a b c = a + b + c -- Correct
 
 copy :: a -> (a, a)
-copy a = (a, a)
+copy a = (a, a) -- Correct
 
 apply :: (a -> b) -> a -> b
-apply f(a) = f(a)  
+apply f(a) = f(a)  -- Correct
 
 -- Exercise 3
 
 second xs = head (tail xs)
-second :: [a] -> a
+second :: [a] -> a -- Correct
 
 swap (x, y) = (y, x)
-swap :: (a, b) -> (b, a)
+swap :: (a, b) -> (b, a) -- Correct
 
 pair x y = (x, y)
-pair :: a -> b -> (a, b)
+pair :: a -> b -> (a, b) -- Correct
 
 double x = x*2
-double :: Num => a -> a
+double :: Num => a -> a -- Correct
 
 pallindrome xs = reverse xs == xs
-pallindrome :: Eq a => [a] -> Bool
+pallindrome :: Eq a => [a] -> Bool -- Correct
 
 twice f x = f (f x)
-twice :: (t -> t) -> t -> t
+twice :: (t -> t) -> t -> t -- Correct
