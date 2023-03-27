10e7d5586635eea833c01d7b8e50095b02f4f339
Jakub Poczatek <kubapoczatek2001@gmail.com>
2023-03-27 22:29:43 +0100

    Stuff

--- Higher Order Functions/exerciseSheet_foldr.hs
+++ Higher Order Functions/exerciseSheet_foldr.hs
@@ -3,21 +3,26 @@ sumsq :: Integral a => a -> a
 sumsq n = foldr op 0 [1..n]
     where op x y = x*x + y
 
+-- Exercise 2
 lengthr :: [Int] -> Int
 lengthr = foldr (\x y -> 1 + y) 0
 
+-- Exercise 3
 minlist :: [Int] -> Int
 minlist = foldr1 min
 
+-- Exercise 4
 myreverse :: [a] -> [a]
 myreverse = foldr (\x y -> y ++ [x]) []
 
+-- Exercise 5
 remove :: Eq a => [a] -> [a] -> [a]
 remove fcs = foldr (\x result -> findElem x fcs ++ result) []
     where findElem x ys 
             | x `elem` ys = []
             | otherwise = [x]
 
+-- Exercise 6
 remdupsr :: Eq a => [a] -> [a]
 remdupsr [] = []
 remdupsr ys = foldr joinr [] ys
